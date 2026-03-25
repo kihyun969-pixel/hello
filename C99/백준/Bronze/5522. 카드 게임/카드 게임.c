9a644c2908398a0aa0d541dc7262671cc0479da5
박기현 <kihyun969@gmail.com>
2026-03-25 12:14:34 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- C99/백준/Bronze/5522. 카드 게임/카드 게임.c
+++ C99/백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -1,24 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    int result = 0;
+	int result = 0;
 
-    for (int i = 0; i < 5; i++) {
-        int a = 0;
-        int flag = 1;
-        
-        while (flag == 1) {
-            scanf("%d", &a);
-            if (a >= 0 && a <= 10) {
-                flag = 0;
-            }
-        }
-        
-        result += a;
-        
-    }
+	for (int i = 0; i < 5; i++) {
+		int a = 0;
+		int flag = 1;
 
-    printf("%d\n", result);
+		while (flag == 1) {
+			scanf("%d", &a);
+			if (a >= 0 && a <= 100) {
+				flag = 0;
+			}
+		}
+		result += a;
+	}
 
-    return 0;
+	printf("%d\n", result);
+
+	return 0;
 }
